ccad45a053f8a4d468d28297d12ac3d20d625bdb
AlanCO97 <aaco9704@gmail.com>
2025-10-12 13:27:25 -0600

    [ADD] - Tests for User domain

---
+++ backend/paycore/src/__tests__/unit/domain/user/User.test.ts
@@ -0,0 +1,56 @@
+// src/__tests__/unit/domain/user/User.test.ts
+import { describe, it, expect } from 'bun:test';
+import { User } from '../../../../domain/user/User';
+
+describe('User Domain Entity', () => {
+    describe('constructor', () => {
+        it('Crear usuario con data valida', async () => {
+            
+            const hashedPassword = await Bun.password.hash('password123')
+            
+            const userData = {
+                name: 'Juan',
+                paternalSurname: 'Pérez',
+                maternalSurname: 'González',
+                email: 'juan@example.com',
+            };
+
+
+            const user = User.create(
+                userData.name,
+                userData.paternalSurname,
+                userData.email,
+                hashedPassword,
+                userData.maternalSurname
+            );
+
+            expect(user.paternalSurname).toBe('Pérez');
+            expect(user.maternalSurname).toBe('González');
+            expect(user.email).toBe('juan@example.com');
+            expect(user.fullName).toBe('Juan Pérez González');
+            expect(user.hashedPassword).toBe(hashedPassword);
+        });
+
+        it('Crear Usuario sin apellido materno', async () => {
+            const userData = {
+                name: 'Juan',
+                paternalSurname: 'Pérez',
+                maternalSurname: null,
+                email: 'juan@example.com',
+                password: 'password123',
+            };
+
+            const hashedPassword = await Bun.password.hash(userData.password)
+
+            const user = User.create(
+                userData.name,
+                userData.paternalSurname,
+                userData.email,
+                hashedPassword,
+                userData.maternalSurname
+            );
+
+            expect(user.fullName).toBe('Juan Pérez');
+        });
+    });
+});
